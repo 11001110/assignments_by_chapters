cb21e69fdace8bb17d00d3757a7228191fcbe047
justin <justinreinmuth@gmail.com>
2019-12-10 14:41:50 -0700

    test

---
+++ methods/exercises.rb
@@ -0,0 +1,20 @@
+#exercise 1
+def greeting(name)
+  "Hey there, " + name + "!"
+end
+
+puts greeting("Justin")
+
+#exercise 2
+#1. => 2
+#2. nil
+#3. "Joe"
+#4. "four"
+#5. nil
+
+#exercise 3
+def multiply(num1, num2)
+  num1 * num2
+end
+
+puts multiply(10, 10)
